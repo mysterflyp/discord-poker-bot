44f225d7565a53149155bd876cf2818b1c422387
mysterflyp <65296367+mysterflyp@users.noreply.github.com>
2025-04-07 09:30:27 +0000

    Ajout couleur pour les cartes

--- poker_game.py
+++ poker_game.py
@@ -15,8 +15,7 @@ MIN_PLAYERS = 1
 
 # Define the classes for the poker game
 class Card:
-    suits = ['♠', '♥', '♦', '♣']
-    #suits = ['♠️', '♥️', '♦️', '♣️']
+    suits = ['♠️', '♥️', '♦️', '♣️']
     values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
     def __init__(self, suit, value):
